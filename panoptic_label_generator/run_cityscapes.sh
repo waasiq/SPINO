82a1221ab9c88480fc9279547d9e3d39b894a0fa
Waasiq Masood <masoodw@informatik.uni-freiburg.de>
2025-06-25 14:11:58 +0200

    Milestone 1 try 1

--- panoptic_label_generator/run_cityscapes.sh
+++ panoptic_label_generator/run_cityscapes.sh
@@ -1,6 +1,11 @@
 eval "$(conda shell.bash hook)"
 conda activate spino
 mkdir -p logs
-python semantic_fine_tuning.py fit --trainer.devices [6] --config configs/semantic_cityscapes.yaml > logs/semantic_cityscapes.txt 2>&1
-python boundary_fine_tuning.py fit --trainer.devices [6] --config configs/boundary_cityscapes.yaml > logs/boundary_cityscapes.txt 2>&1
-python instance_clustering.py test --trainer.devices [6,7] --config configs/instance_cityscapes.yaml > logs/instance_cityscapes.txt 2>&1
+python semantic_fine_tuning.py fit --trainer.devices [0] --config configs/semantic_cityscapes.yaml > logs/semantic_cityscapes.txt 2>&1
+python boundary_fine_tuning.py fit --trainer.devices [0] --config configs/boundary_cityscapes.yaml > logs/boundary_cityscapes.txt 2>&1
+python instance_clustering.py test --trainer.devices [0] --config configs/instance_cityscapes.yaml > logs/instance_cityscapes.txt 2>&1
+
+ln -s /home/data/cityscapes/camera/ /home/data/cityscapes_pseudolabels/
+ln -s /home/data/cityscapes/leftImg8bit/ /home/data/cityscapes_pseudolabels/
+ln -s /home/data/cityscapes/leftImg8bit_sequence/ /home/data/pastel/cityscapes_pseudolabels/
+# python -m panoptic_segmentation_model.scripts_dev.evaluate_labels --dataset_name cityscapes --gpu_id 0 /home/data/cityscapes /home/data/cityscapes_pseudolabels/
